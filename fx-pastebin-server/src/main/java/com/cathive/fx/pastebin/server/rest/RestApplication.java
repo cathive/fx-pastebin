500a4efe7433bd7317f3e3d758c5c02c627329d8
Benjamin P. Jung <headcr4sh@gmail.com>
2014-04-03 12:05:59 +0200

    Move RESTful application from web.xml to Java class.

---
+++ fx-pastebin-server/src/main/java/com/cathive/fx/pastebin/server/rest/RestApplication.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2014 The Cat Hive Developers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cathive.fx.pastebin.server.rest;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Startup;
+import javax.inject.Singleton;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Representation of the RESTful application that will be deployed as Servlet by
+ * the JAX-RS runtime.
+ * @author Benjamin P. Jung
+ */
+@Singleton
+@Startup
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+
+    /** Logger for this class. */
+    private final Logger LOGGER = Logger.getLogger(RestApplication.class.getName());
+
+    @PostConstruct
+    protected void initialize() {
+        LOGGER.log(Level.CONFIG, "Initializing RESTful application...");
+    }
+
+}
