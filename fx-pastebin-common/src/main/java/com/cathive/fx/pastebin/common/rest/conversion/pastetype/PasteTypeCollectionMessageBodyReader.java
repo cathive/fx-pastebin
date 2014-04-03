2963fb454959554bc950eaf76965a4ecf7d21350
Benjamin P. Jung <headcr4sh@gmail.com>
2014-04-03 22:09:38 +0200

    Add message body reader for collections of paste types.

---
+++ fx-pastebin-common/src/main/java/com/cathive/fx/pastebin/common/rest/conversion/pastetype/PasteTypeCollectionMessageBodyReader.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2014 The Cat Hive Developers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cathive.fx.pastebin.common.rest.conversion.pastetype;
+
+import com.cathive.fx.pastebin.common.model.PasteType;
+import com.cathive.fx.pastebin.common.rest.conversion.common.AbstractCollectionMessageBodyReader;
+
+import javax.json.Json;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.ext.Providers;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author Benjamin P. Jung
+ */
+@Provider
+@Consumes
+public class PasteTypeCollectionMessageBodyReader extends AbstractCollectionMessageBodyReader<PasteType> {
+
+    @Context
+    private Providers providers;
+
+    public PasteTypeCollectionMessageBodyReader() {
+        super(PasteType.class);
+    }
+
+
+    @Override
+    public Collection<PasteType> readFrom(Class<Collection<PasteType>> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, String> httpHeaders, InputStream entityStream) throws IOException, WebApplicationException {
+        final List pasteTypes = new ArrayList<>();
+        final MessageBodyReader<PasteType> reader = providers.getMessageBodyReader(PasteType.class, PasteType.class, annotations, mediaType);
+        Json.createReader(entityStream).readArray().forEach((jsonValue) -> {
+            try {
+                pasteTypes.add(reader.readFrom(PasteType.class, PasteType.class, annotations, mediaType, httpHeaders, entityStream));
+            } catch (final IOException e) {
+                throw new IllegalStateException(e);
+            }
+        });
+        return pasteTypes;
+    }
+
+}
