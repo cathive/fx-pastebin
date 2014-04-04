5d91fc3db6f11be3892a986e2c09306f32672911
Benjamin P. Jung <headcr4sh@gmail.com>
2014-04-04 12:46:01 +0200

    Remove remaining 'diff' marker.

--- fx-pastebin-server/src/main/java/com/cathive/fx/pastebin/server/configuration/Fixture.java
+++ fx-pastebin-server/src/main/java/com/cathive/fx/pastebin/server/configuration/Fixture.java
@@ -27,14 +27,8 @@ import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.inject.Inject;
-<<<<<<< HEAD
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-=======
 import java.io.IOException;
 import java.io.InputStream;
->>>>>>> Use java.util.Properties to parse .properties file.
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
