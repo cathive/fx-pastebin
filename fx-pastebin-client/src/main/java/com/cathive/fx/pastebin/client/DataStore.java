020b02cd8cb5e12a74cec1fd535f3f6a638e37fd
Benjamin P. Jung <headcr4sh@gmail.com>
2014-04-13 22:19:49 +0200

    Remove client-side persistence.xml
    transaction-type default to JTA in JavaEE environments and is set to RESOURCE_LOCAL when running JavaSE, so there is no need for different persistent unit definitions.

--- fx-pastebin-client/src/main/java/com/cathive/fx/pastebin/client/DataStore.java
+++ fx-pastebin-client/src/main/java/com/cathive/fx/pastebin/client/DataStore.java
@@ -28,7 +28,7 @@ import java.util.Map;
 public class DataStore {
 
     /** Name of the persistence unit to be used when working with pastes. */
-    public static final String PERSISTENCE_UNIT_NAME = "fx-pastebin-client";
+    public static final String PERSISTENCE_UNIT_NAME = "fx-pastebin";
 
     private final Map<String, String> persistenceProperties;
 
